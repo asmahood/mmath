0a93fbc27bc173c521bd632f178246b04f76021f
Adam Mahood <mahood.adam@gmail.com>
2019-10-11 23:00:04 -0400

    [src][build] committing the starting files

---
+++ matrix/include/matrix.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace mmath {
+
+/**
+ * Implementation of a matrix type to perform matrix math and operations.
+ * 
+ * Implements matrix operations such as row reducing, matrix-vector multiplication, 
+ * matrix-matrix multiplication and others.
+ */
+template<typename T>
+class Matrix {
+  private:
+
+  public:
+};
+
+} // namespace mmath
